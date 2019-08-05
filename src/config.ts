267f4a00af197f37eb0c2ee506ef5047790f5147
Glen Burchfield <glen.burchfield@baker-taylor.com>
2019-08-05 10:10:27 -0400

    config for docker dev env

--- src/config.ts
+++ src/config.ts
@@ -3,12 +3,12 @@ const environment: string = process.env.NODE_ENV || 'dev'
 
 const config: ConfigParent = {
     dev: {
-        port: 4002,
-        secret: "development_secret",
-        cluster_uri: "mongodb+srv://development-b96of.mongodb.net/test?retryWrites=true&w=majority",
-        db_name: "Profile",
-        db_user: "ProfileServiceUser",
-        db_password: "Password"
+        port: parseInt(process.env.PORT),
+        secret: process.env.SECRET_KEY,
+        cluster_uri: process.env.MONGODB_URL,
+        db_name: process.env.DB_NAME,
+        db_user: process.env.DB_USER,
+        db_password: process.env.DB_PASSWORD
     },
     prod: {
         port: parseInt(process.env.PORT),
